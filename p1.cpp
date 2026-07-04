8b55d3ef9c550a1fb85fe8993cd21fab1515ab41
Parth Bhogan <parthbhogan2007@gmail.com>
2026-07-04 20:04:48 +0530

    Day 1 - Variables and Output

---
+++ p1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {             
+string name ="Parth";
+int age =18;
+float height = 5.7;
+string branch="computer engineering";
+
+
+
+cout << "Name: " << name << endl;
+cout << "Age: " << age << endl;
+cout << "Height: " << height << endl;
+cout << "Branch: " << branch << endl;
+
+    return 0;
+}
