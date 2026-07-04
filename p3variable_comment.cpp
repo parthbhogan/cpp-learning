910fcecb6550860fee7381cfd33ecd8ac22cddc0
Parth Bhogan <parthbhogan2007@gmail.com>
2026-07-04 22:39:19 +0530

    variables and comments

---
+++ p3variable_comment.cpp
@@ -0,0 +1,42 @@
+
+//variables---->
+//variable is a named storage location in memory
+//Each variable in C++ has a specific type,
+//types of variables-->
+int x = 5; // integer variable-->stores whole numbers
+float y = 8.07; // floating-point variable-->stores decimal numbers
+double z = 8.72007; // double-precision floating-point variable-->stores more precise decimal numbers
+char c = 'A'; // character variable-->stores single characters
+bool b = true; // boolean variable-->stores true or false values
+
+
+/*comments----->
+used to explain the code and make it more readable
+compiler ignores comments while executing the code
+there are two types of comments in C++:
+1. Single-line comments: used to comment a single line of code--->"//comment"
+2. Multi-line comments: used to comment multiple lines of code--->/* comment */
+
+
+//code----->
+#include <iostream>
+using namespace std;   
+main ()
+{
+int a = 10; // integer variable
+float b = 6.77; // floating-point variable  
+double c = 8.72007; // double-precision floating-point variable
+char d = 'A'; // character variable
+bool e = true; // boolean variable
+/*  example code for intrgers and comments */
+cout << "\nThe value of a is: " << a << endl; 
+cout << "\nThe value of b is: " << b << endl;
+cout << "\nThe value of c is: " << c << endl; 
+cout << "\nThe value of d is: " << d << endl; 
+cout << "\nThe value of e is: " << e << endl; 
+
+    return 0;
+}
+
+
+
