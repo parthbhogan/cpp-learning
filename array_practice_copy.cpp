fe73f67f5698d5a9b4af6487c7999701b93722a6
Parth Bhogan <parthbhogan2007@gmail.com>
2026-08-28 03:06:28 +0530

    array copy

---
+++ array_practice_copy.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={};
+    int i;
+    for( i=0;i<5;i++){
+    cout<<"enter 5 numbers:";
+    cin>>arr[i];
+    }
+    int arr2[5]={};
+
+    for(int j=0;j<5;j++){
+    arr2[j]=arr[j];
+    }
+return 0;
