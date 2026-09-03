8cdd017da6c0e6c4316b6aaefa5a88d0b02db7a4
Parth Bhogan <parthbhogan2007@gmail.com>
2026-09-04 00:18:48 +0530

    Add move zeros to end array practice

---
+++ arrays/array_practice_move_zeros_end.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[7]={};
+    for(int i=0;i<7;i++){
+        cout<<"enter elementes: ";
+        cin>>arr[i];
+    }
+for(int j=0;j<7;j++){
+    if(arr[j]==0){
+
+        for(int i=j;i<6;i++){
+            int temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+        }
+
+    }
+}
+for(int j=0;j<7;j++){
+    cout<<arr[j]<<" ";
+}
+return 0;
+}
