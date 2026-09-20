00054548a2fa52ba567821a5ad2e4c325f8c04e0
Parth Bhogan <parthbhogan2007@gmail.com>
2026-09-20 23:55:15 +0530

    reverse each word practice

--- strings/string_practice_reverse_each_word.cpp
+++ strings/string_practice_reverse_each_word.cpp
@@ -7,16 +7,22 @@ int main(){
     getline(cin,str);
         int start=0;
     for(int i=0;i<str.length();i++){
-        if(str[i]==' '|| i==str.length()-1){
+        if(str[i]==' '){
 
             for(int j = i- 1; j >= start; j--){
-                start=str[i+1];
 
+                    cout<<str[j];
+            }
+            cout<<" ";
+            start=i+1;
+            
+        }
+        else if(i==str.length()-1){
+            for(int j = i; j >= start; j--){
             cout<<str[j];
             }
+
         }
-
-
 }
 return 0;
 }
