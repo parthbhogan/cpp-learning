1ad4270ed0a0a44810f73b856d273e2226774dd8
Parth Bhogan <parthbhogan2007@gmail.com>
2026-09-24 23:23:15 +0530

    Add first repeated character practice

---
+++ strings/string_practice_first_repeated_character.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str;
+    cout<<"enter your string: ";
+    getline(cin,str);
+    bool found=false;
+    for(int i=0;i<str.length();i++){
+        int count=0;
+        for(int j=0;j<str.length();j++){
+            if(str[i]==str[j]&&i!=j){
+                count ++;
+            }
+        }
+        if(count!=0){
+            cout<<str[i];
+            found=true;
+            break;
+        }
+    }
+    if(found==false){
+        cout<<"no any repeating character !!";
+    }
+    return 0;
+}
